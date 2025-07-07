e5bf445c6953ae8376cc2632486462dc1955f12a
Rihyun Kang <gangri20040512@gmail.com>
2025-07-08 00:30:09 +0900

    Initial commit

---
+++ PhonesonalTrainer/PhonesonalTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhonesonalTrainerApp.swift
+//  PhonesonalTrainer
+//
+//  Created by 강리현 on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PhonesonalTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
